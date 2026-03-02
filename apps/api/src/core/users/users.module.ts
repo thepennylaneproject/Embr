9c5dc7e2abb9b831e3a3246edf1c6d0a1f44dfc1
Sarah Sahl <Sarah@thepennylaneproject.org>
2026-03-02 09:33:22 -0600

    TS and import errors

--- apps/api/src/core/users/users.module.ts
+++ apps/api/src/core/users/users.module.ts
@@ -2,8 +2,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UploadModule } from '../upload/upload.module';
 
 @Module({
+  imports: [UploadModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
